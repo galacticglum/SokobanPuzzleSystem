8a35cc623dc65c95979864c472b74497f12d2006
GalacticGlum <nerftactic09@gmail.com>
2017-12-11 18:29:05 -0500

    Project files.

---
+++ Assets/Game/Scripts/Editor/SokobanPuzzleControllerEditor.cs
@@ -0,0 +1,63 @@
+﻿using UnityEditor;
+using UnityEditor.IMGUI.Controls;
+using UnityEditorInternal;
+using UnityEngine;
+
+[CustomEditor(typeof(SokobanPuzzleController))]
+public class SokobanPuzzleControllerEditor : Editor
+{
+    private static GUIContent EditModeButton => EditorGUIUtility.IconContent("EditCollider");
+
+    private SerializedPropertyManager propertyManager;
+    private BoxBoundsHandle boxBoundsHandle;
+
+    private void OnEnable()
+    {
+        propertyManager = new SerializedPropertyManager(serializedObject);
+        boxBoundsHandle = new BoxBoundsHandle
+        {
+            axes = PrimitiveBoundsHandle.Axes.X | PrimitiveBoundsHandle.Axes.Y
+        };
+    }
+
+    public override void OnInspectorGUI()
+    {
+        EditMode.DoEditModeInspectorModeButton(EditMode.SceneViewEditMode.Collider, "Edit Bounds", EditModeButton, () => new Bounds(boxBoundsHandle.center, boxBoundsHandle.size), this);
+
+        propertyManager["size"].vector2Value = EditorGUILayout.Vector2Field(new GUIContent("Size"), 
+            propertyManager["size"].vector2Value);
+
+        propertyManager["offset"].vector2Value = EditorGUILayout.Vector2Field(new GUIContent("Offset"), propertyManager["offset"].vector2Value);
+
+        propertyManager["topDownGrid"].boolValue = EditorGUILayout.Toggle(new GUIContent("Is Topdown Grid"),
+            propertyManager["topDownGrid"].boolValue);
+
+        propertyManager.Target.ApplyModifiedProperties();
+    }
+
+    private void OnSceneGUI()
+    {
+        SokobanPuzzleController controller = (SokobanPuzzleController) target;
+        using (new Handles.DrawingScope(controller.GridViewMatrix))
+        {
+            boxBoundsHandle.center = propertyManager["offset"].vector2Value;
+            boxBoundsHandle.size = propertyManager["size"].vector2Value;
+
+            EditorGUI.BeginChangeCheck();
+            boxBoundsHandle.DrawHandle();
+            if (!EditorGUI.EndChangeCheck()) return;
+
+            Undo.RecordObject((SokobanPuzzleController)target,
+                $"Modify {ObjectNames.NicifyVariableName(target.GetType().Name)}");
+
+            Vector2 oldSize = propertyManager["size"].vector2Value;
+            propertyManager["size"].vector2Value = boxBoundsHandle.size;
+            if (propertyManager["size"].vector2Value != oldSize)
+            {
+                //propertyManager["offset"].vector2Value = boxBoundsHandle.center;
+            }
+        }
+
+        propertyManager.Target.ApplyModifiedProperties();
+    }
+}
---
+++ Assets/Game/Scripts/SokobanPuzzleController.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+public class SokobanPuzzleController : MonoBehaviour
+{
+    public Matrix4x4 GridViewMatrix 
+    {
+        get
+        {
+            Vector3 rotation = new Vector3(0, 0, 0);
+            Vector3 position = transform.position;
+
+            if (!topDownGrid)
+            {
+                return Matrix4x4.Rotate(Quaternion.Euler(rotation)) * Matrix4x4.Translate(position);
+            }
+
+            rotation.x = 90;
+
+            float oldY = position.y;
+            position.y = position.z;
+            position.z = -oldY;
+
+            return Matrix4x4.Rotate(Quaternion.Euler(rotation)) * Matrix4x4.Translate(position);
+        }
+    }
+
+    [SerializeField]
+    private Vector2 size = Vector2.one;
+    [SerializeField]
+    private Vector2 offset;
+    [SerializeField]
+    private bool topDownGrid = true;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.matrix = GridViewMatrix;
+
+        int width = Mathf.FloorToInt(size.x / 2f);
+        int height = Mathf.FloorToInt(size.y / 2f);
+
+        // draw the horizontal lines
+        for (int x = -width; x < width + 1; x++)
+        {
+            InitializeGizmoColour(x);
+
+            Vector3 a = new Vector3(x, -height, 0);
+            Vector3 b = new Vector3(x, height, 0);
+
+            Gizmos.DrawLine(a, b);
+        }
+
+        // draw the vertical lines
+        for (int y = -height; y < height + 1; y++)
+        {
+            InitializeGizmoColour(y);
+
+            Vector3 a = new Vector3(-width, y, 0);
+            Vector3 b = new Vector3(width, y, 0);
+
+            Gizmos.DrawLine(a, b);
+        }
+    }
+
+    private static void InitializeGizmoColour(int value) => Gizmos.color = value == 0 ? Color.white : Color.green;
+}
